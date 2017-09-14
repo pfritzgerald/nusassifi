12f88e8042b2831d73c84290d2548bdf48341c76
Fritz Previlon <previlon.f@husky.neu.edu>
2017-09-14 11:31:51 -0400

    test for bfs

--- scripts/specific_params.py
+++ scripts/specific_params.py
@@ -43,12 +43,12 @@ import common_params as p
 # campaign. This is essentially the maximum number of injections one can run
 # per instruction group (IGID) and bit-flip model (BFM).
 # 
-NUM_INJECTIONS = 10000
+NUM_INJECTIONS = 10#000
 
 
 # Specify how many injections you want to perform per IGID and BFM combination. 
 # Only the first THRESHOLD_JOBS will be selected from the generated NUM_INJECTIONS.
-THRESHOLD_JOBS = 10000 # test
+THRESHOLD_JOBS = 10#000 # test
 
 # THRESHOLD_JOBS sould be <= NUM_INJECTIONS
 assert THRESHOLD_JOBS <= NUM_INJECTIONS
@@ -88,12 +88,12 @@ igid_bfm_map = {
 
 apps = {
 #	'b+tree': ['rodinia', 'b+tree.out', 3],
-	'lavaMD': ['rodinia','lavaMD',3],
-	'nw': ['rodinia','nw', 3],
-	'gaussian': ['rodinia', 'gaussian', 2],
+	#'lavaMD': ['rodinia','lavaMD',3],
+	#'nw': ['rodinia','nw', 3],
+	#'gaussian': ['rodinia', 'gaussian', 2],
 #	'backprop': ['rodinia', 'backprop', 4],
-	'hotspot': ['rodinia', 'hotspot',5],
-	'kmeans': ['rodinia', 'kmeans',5],
+	#'hotspot': ['rodinia', 'hotspot',5],
+	#'kmeans': ['rodinia', 'kmeans',5],
 	'bfs': ['rodinia', 'bfs', 10],
 #	'matrixMul': ['example', 'matrixMul', 2],
 #	'bfs': ['parboil', 'bfs', 10],
